3e259a6b3e2f91a4b1ae2a47c9921a8ba18e8814
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-25 02:20:23 -0800

    Instructor solution.

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,22 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button @click="selectedComponent='appBlue'" class="btn btn-primary">Load Blue Template</button>
-                <button @click="selectedComponent='appGreen'" class="btn btn-success">Load Green Template</button>
-                <button @click="selectedComponent='appRed'" class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
                 <hr>
-                <component :is="selectedComponent">{{selectedComponent}}</component>
+                <component :is="selectedComponent">
+                    <p>This is the Content</p>
+                </component>
+                <!--<app-blue>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-blue>-->
+                <!--<app-green>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-green>-->
+                <!--<app-red>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-red>-->
             </div>
         </div>
     </div>
@@ -19,9 +30,9 @@
     import Red from './components/Red.vue';
 
     export default {
-        data: function(){
+        data: function() {
           return {
-            selectedComponent:"appBlue",
+              selectedComponent: 'appBlue'
           }
         },
         components: {
